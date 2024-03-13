087f524d2a06da0307b997f075e41d64bf355635
toddharper <todd@businesstechbff.com>
2024-03-12 20:51:22 -0500

    1) Updated file structure (will add documentation to YouTrack Knowledge Base) 2) Moved Templates directory to root and adjusted settings to point there 3) Added base CSS files to aid with front end development #TRU-41 Testing @lidiya.kuznetsova @samuel.richter I'll have a pull request for you momentarily.

--- trips_r_us/settings.py
+++ trips_r_us/settings.py
@@ -10,11 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Project Wide Constants
+# Set constants here that can be used in any file throughout the project
+# To implement in a file, "from django.conf import settings", then variable_name = settings.CONSTANT_NAME
+APP_NAME = 'Trips-R-Us'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -60,7 +65,7 @@ ROOT_URLCONF = 'trips_r_us.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # Points Django to travel-companion/templates rather than nesting this directory deeper
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
