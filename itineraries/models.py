015b1e6862d9920ad80adaf14125166959346f1b
toddharper <todd@businesstechbff.com>
2024-04-02 11:44:51 -0500

    1) Updated Day and Activity models to reflect new data structure 2) Created Itineary model #TRU-103 @samuel.richter @lidiya.kuznetsova @jashonMenya @ashley.spriggle I will leave some documentation about how to write to and read from these models in the Knowledge Base

--- itineraries/models.py
+++ itineraries/models.py
@@ -2,38 +2,31 @@ from django.db import models
 from django.contrib import admin
 from django.contrib.auth.models import User
 
-class Traveler(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=15, null=True, blank=True)
-
-    def itineraries(self):
-        return self.itinerary_set.all()
-    
-    def __str__(self):
-        return self.user.username
-    
-    @admin.display(ordering='user__first_name')
-    def first_name(self):
-        return self.user.first_name
-    
-    @admin.display(ordering='user__last_name')
-    def last_name(self):
-        return self.user.last_name
-    
-    class Meta:
-        ordering = ['user__first_name', 'user__last_name']
-
-class PreferencesFormResponse(models.Model):
-    destination = models.CharField(max_length=100)
+class Itinerary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    destination = models.CharField(max_length=200)
     start_date = models.DateField()
     end_date = models.DateField()
-    travelers = models.IntegerField()
+    description = models.TextField(blank=True)
+    num_travelers = models.IntegerField(default=1)
 
     def __str__(self):
-        return f"Destination: {self.destination}, Travelers: {self.travelers}"
-        
+        return self.title
+    
+class Day(models.Model):
+    itinerary = models.ForeignKey(Itinerary, on_delete=models.CASCADE)
+    date = models.DateField()
+    day_number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.itinerary.title} - Day {self.day_number}"
+    
 class Activity (models.Model):
+    day = models.ForeignKey(Day, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
     # Add other fields as needed
     editSummary = models.CharField(max_length=255) # summary of the place
     name = models.CharField(max_length=255) #name of place
@@ -58,32 +51,14 @@ class Activity (models.Model):
     southwest = models.CharField(max_length=255) # LatLng
     website = models.CharField(max_length=255) # Authoritative website for the place, like the business homepage.
 
-########## FOLLOWING ARE MERELY PLACEHOLDERS THAT SHOULD BE REVISED AND MOVED ABOVE THIS LINE WHEN READY ##########
+    def __str__(self):
+        return self.name
 
-class Day(models.Model):
-    date = models.DateField()
-    activities = models.ManyToManyField(Activity, through='ActivityAssignment')
+class PreferencesFormResponse(models.Model):
+    destination = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    travelers = models.IntegerField()
 
     def __str__(self):
-        return self.date.strftime('%Y-%m-%d')
-    
-class ActivityAssignment(models.Model):
-    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
-    day = models.ForeignKey(Day, on_delete=models.CASCADE)
-    start_time = models.TimeField()
-
-    class Meta:
-        ordering = ['start_time']
-    
-    def __str__(self):
-        return f"{self.activity.title} on {self.day.date.strftime('%Y-%m-%d')} at {self.start_time.strftime('%H:%M')}"
-    
-class Itinerary(models.Model):
-    traveler = models.ForeignKey(Traveler, on_delete=models.CASCADE, null=True, blank=True)
-    days = models.ManyToManyField(Day)
-
-    def __str__(self):
-        if self.traveler:
-            return f"Itinerary for {self.traveler.user.username}"
-        else:
-            return "Unassigned Itinerary"
+        return f"Destination: {self.destination}, Travelers: {self.travelers}"
