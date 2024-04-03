5a2b0ca11c7f39d5100be87a5a162aeb45417758
JashonMenya <jjmenya14@gmail.com>
2024-04-03 18:08:19 -0500

    using UserReg and added email

--- itineraries/views.py
+++ itineraries/views.py
@@ -5,6 +5,7 @@ from .forms import PreferencesForm
 from .models import PreferencesFormResponse
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegistrationForm 
 
 def index(request):
     if request.method == 'POST':
@@ -40,16 +41,15 @@ Registers a new user and redirects to the home page.
 
 When a new user submits registration information, it's saved to the database, and then the user is redirected to the home page.
 '''
-
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
     else:
-        form = UserCreationForm()
-    return render(request, 'header-main.html', {'form': form})
+        form = UserRegistrationForm()
+    return render(request, 'home', {'form': form})
 
 
 
@@ -58,7 +58,6 @@ Logs in a user and redirects to the home page.
 
 When a user submits login credentials, it checks if the provided username and password are valid. If they are, the user is logged in and redirected to the home page.
 '''
-
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
